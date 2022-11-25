b84cdf6ec8b77401a8b54849f1e265671474fd9a
Flavio Piccinelli <fpiccinelli@jmawireless.com>
2022-11-25 16:07:02 +0100

    Use logging library

--- cmd/idrac_exporter/main.go
+++ cmd/idrac_exporter/main.go
@@ -3,26 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/mrlhansen/idrac_exporter/internals/config"
+	"github.com/mrlhansen/idrac_exporter/internals/logging"
 	"github.com/mrlhansen/idrac_exporter/internals/promexporter"
 )
 
+var logger = logging.NewLogger().Sugar()
+
 func main() {
+	var verbose bool
 	var configFile string
 	var metricsPrefix string
 
+	flag.BoolVar(&verbose, "verbose", false, "Set verbose logging")
 	flag.StringVar(&configFile, "config", "/etc/prometheus/idrac.yml", "Path to idrac exporter configuration file")
 	flag.StringVar(&metricsPrefix, "prefix", "idrac_", "Prefix to prepend to metrics names")
 	flag.Parse()
 
 	config.ReadConfigFile(configFile)
 
+	if verbose {
+		logging.SetVerboseLevel()
+	}
+
 	http.HandleFunc("/metrics", promexporter.MetricsHandler(metricsPrefix))
 	bind := fmt.Sprintf("%s:%d", config.Config.Address, config.Config.Port)
+
+	logger.Infof("Server listening on %s", bind)
+
 	if err := http.ListenAndServe(bind, nil); err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 }
