91291ef51caa14c8626e6a973fea2bf02647d9fe
Flavio Piccinelli <fpiccinelli@jmawireless.com>
2022-11-25 15:27:01 +0100

    Cleaned configs structs and code

--- internals/config/config.go
+++ internals/config/config.go
@@ -2,34 +2,47 @@ package config
 
 import (
 	"encoding/base64"
-	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 
 	"gopkg.in/yaml.v2"
 )
 
 type HostConfig struct {
-	Username        string `yaml:"username"`
-	Password        string `yaml:"password"`
-	Hostname        string
-	Token           string
-	Metrics         string
-	Initialized     bool
-	Reachable       bool
-	Retries         uint32
-	SystemEndpoint  string
-	ThermalEndpoint string
-	PowerEndpoint   string
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Hostname string
+	Token    string
 }
 
 type RootConfig struct {
+	mu sync.Mutex
+
 	Address string                 `yaml:"address"`
-	Port    uint32                 `yaml:"port"`
+	Port    uint                   `yaml:"port"`
 	Metrics []string               `yaml:"metrics"`
-	Timeout uint32                 `yaml:"timeout"`
-	Retries uint32                 `yaml:"retries"`
+	Timeout uint                   `yaml:"timeout"`
+	Retries uint                   `yaml:"retries"`
 	Hosts   map[string]*HostConfig `yaml:"hosts"`
+}
+
+func (c *RootConfig) GetHostCfg(target string) *HostConfig {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	hostCfg, ok := c.Hosts[target]
+	if !ok {
+		hostCfg = &HostConfig{
+			Hostname: target,
+			Username: c.Hosts["default"].Username,
+			Password: c.Hosts["default"].Password,
+			Token:    c.Hosts["default"].Token,
+		}
+		c.Hosts[target] = hostCfg
+	}
+
+	return hostCfg
 }
 
 var Config RootConfig
@@ -57,24 +70,22 @@ func validateMetrics(name string) bool {
 }
 
 func parseError(s0, s1 string) {
-	log.Printf("Error parsing configuration file: %s: %s", s0, s1)
-	os.Exit(1)
+	log.Fatalf("Error parsing configuration file: %s: %s", s0, s1)
 }
 
 func ReadConfigFile(fileName string) {
-	yamlFile, err := ioutil.ReadFile(fileName)
+	yamlFile, err := os.Open(fileName)
 	if err != nil {
-		log.Printf("Error reading configuration file: %s\n", err)
-		os.Exit(1)
+		log.Fatalf("Error opening configuration file %s: %s", fileName, err)
 	}
 
-	err = yaml.Unmarshal(yamlFile, &Config)
+	err = yaml.NewDecoder(yamlFile).Decode(&Config)
+	yamlFile.Close()
 	if err != nil {
-		log.Printf("Error parsing configuration file: %s\n", err)
-		os.Exit(1)
+		parseError(fileName, err.Error())
 	}
 
-	if len(Config.Address) == 0 {
+	if Config.Address == "" {
 		Config.Address = "0.0.0.0"
 	}
 
@@ -105,17 +116,15 @@ func ReadConfigFile(fileName string) {
 	}
 
 	for k, v := range Config.Hosts {
-		if len(v.Username) == 0 {
+		if v.Username == "" {
 			parseError("missing username for host", k)
 		}
-		if len(v.Password) == 0 {
+		if v.Password == "" {
 			parseError("missing password for host", k)
 		}
 
 		data := []byte(v.Username + ":" + v.Password)
 		v.Token = base64.StdEncoding.EncodeToString(data)
 		v.Hostname = k
-		v.Initialized = false
-		v.Retries = 0
 	}
 }
